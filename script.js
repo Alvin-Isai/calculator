e780e8ab9ed51ac24c3c397c8476a0e15c546961
Alvin <alvinlopez76@yahoo.com>
2020-08-10 22:40:00 -0700

    Completed rewrote script and implemented new array eval method and finishing touches

--- script.js
+++ script.js
@@ -1,87 +1,58 @@
-const screen = document.querySelector('#screen')
-const numButtons = document.querySelectorAll('[data-selection]')
-const operaterButtons = document.querySelectorAll('#operator')
-const equals = document.querySelector('#equals')
+const screen = document.querySelector('#screen');
+const numButtons = document.querySelectorAll('[data-selection]');
+const operaterButtons = document.querySelectorAll('[data-key]');
+const equals = document.querySelector('[data-equals]');
+const memory = document.querySelector('[data-ac]');
+const backspace = document.querySelector('[data-backspace]')
 
 let DISPLAYVALUE = ''
-let FIRSTNUMBER = ''
 let SECONDNUMBER = ''
-let OPERATOR = ''
+let array = []
 
+numButtons.forEach(numButton => {
+    numButton.addEventListener('click', () => {
+        const number = numButton.dataset.selection
+        display(number)
+    });
+});
+
+function display(number) {
+    if (number === '.' && DISPLAYVALUE.includes('.')) return;
+    DISPLAYVALUE += number;
+    screen.innerHTML = DISPLAYVALUE;
+};
 
 operaterButtons.forEach(operaterButton => {
     operaterButton.addEventListener('click', e => {
         OPERATOR = operaterButton.dataset.key
+        array.push(DISPLAYVALUE)
+        array.push(OPERATOR)
         SECONDNUMBER = DISPLAYVALUE
         DISPLAYVALUE = ''
     });
 });
 
-equals.addEventListener('click', e => {
-    operate(OPERATOR, FIRSTNUMBER, SECONDNUMBER);
+equals.addEventListener('click', () => {
+    array.push(DISPLAYVALUE)
     screen.innerHTML = DISPLAYVALUE;
+    operate(array)
+    
 });
 
-
-
-numButtons.forEach(numButton => {
-    numButton.addEventListener('click', e => {
-        const number = numButton.dataset.selection
-        display(number)  
-    });
+backspace.addEventListener('click', () => {
+    let index = DISPLAYVALUE.length;
+    DISPLAYVALUE = DISPLAYVALUE.slice(0, index - 1)
+    screen.innerHTML = DISPLAYVALUE
 });
 
-function display(input) {
-    FIRSTNUMBER = input
-    DISPLAYVALUE += input;
-    screen.innerHTML = DISPLAYVALUE;
-};
+memory.addEventListener('click', e => {
+    DISPLAYVALUE = ''
+    SECONDNUMBER = ''
+    array = []
+    screen.innerHTML = DISPLAYVALUE
+});
 
-
-
-
-function operate(operater, a, b) {
-    if (operater === '+') {
-        let num1 = parseFloat(a);
-        let num2 = parseFloat(b)
-        DISPLAYVALUE = add(num1, num2)
-    }
-    else if (operater === '-') {
-        DISPLAYVALUE = subtract(a, b);
-    }
-    else if (operater === '*') {
-        DISPLAYVALUE = multiply(a, b);
-    }
-    else if (operater === '**') {
-        DISPLAYVALUE = power(a, b);
-    }
-    else if (operater === '/') {
-        DISPLAYVALUE = divide(a, b)
-    }
-    else if (operater === '!') {
-        DISPLAYVALUE = factorial(a);
-    }
-}
-
-
-function add(a, b) {
-    return a + b
-}
-function subtract(a, b) {
-    return a - b
-}
-function divide(a, b) {
-    return a / b
-}
-function multiply(a, b) {
-    return a * b
-}
-function power(a, b) {
-    return a ** b
-}
-function factorial(x) {
-    if (x === 0) {
-        return 1;
-    }
-    return x * factorial(x - 1);
+function operate(array) {
+    let product = eval(array.join(' '))
+    screen.innerHTML = product
 }
